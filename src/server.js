b74fff37fda4d5b7c482a33d3b714f9fdc688f9f
Jorge Tomé <jorge.tomejr@gmail.com>
2023-09-06 11:46:51 -0400

    created AppError

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
+const AppError = require("./utils/AppError")
 const express = require("express")
 const app = express()
 
-app.use(express.json())
+const routes = require("./routes/index.js")
 
-app.post("/users", (request, response) => {
-    const {name, email, password} = request.body
-    response.json( {name, email, password} )
+app.use(express.json())
+app.use(routes)
+
+app.use(( error, request, response, next )=>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
 })
 
 const PORT = 3333
---
+++ src/utils/AppError.js
@@ -0,0 +1,14 @@
+/*Padronizar os erros e mensagens */
+
+
+class AppError{
+    message;
+    statusCode;
+
+    constructor(message, statusCode = 400){
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = AppError
