6d7845793094cf083586ff036c421740daf81b3a
Jorge Tomé <jorge.tomejr@gmail.com>
2023-09-11 10:30:15 -0400

    created app node

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,8 +2,8 @@ const AppError = require("../utils/AppError")
 
 class UsersController {
     create(request, response){
-        const {name, email, password} = request.bodyss
-        
+        const {name, email, password} = request.body
+
         if(!name){
             throw new AppError("Nome é obrigatório")
         }
